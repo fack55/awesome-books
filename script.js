86dbfe40690c8b22da3103b25f3796ef921f9fb7
Nedramrez <zermarden@gmail.com>
2023-01-13 16:40:33 +0600

    Rewrite the code using my own code from the previous attempt of studying this module.

--- script.js
+++ script.js
@@ -1,103 +1,99 @@
-// select the items
-const form = document.querySelector('#form');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const bookList = document.getElementById('books-list');
-
-class Books {
+/* eslint-disable max-classes-per-file */
+class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
+}
 
-  storeData() {
-    const bookTitle = title.value;
-    const bookAuthor = author.value;
-    const book = new Books(bookTitle, bookAuthor);
-    // Check if local storage is empty
-    if (localStorage.getItem('books') === null) {
-      const bookContainer = [];
-      bookContainer.push(book);
-      localStorage.setItem('books', JSON.stringify(bookContainer));
+class Storage {
+  static getBooks() {
+    let collection;
+    if (localStorage.getItem('collection') === null) {
+      collection = [];
     } else {
-      const bookContainerStr = localStorage.getItem('books');
-      const bookArray = JSON.parse(bookContainerStr);
-      bookArray.push(book);
-      localStorage.setItem('books', JSON.stringify(bookArray));
+      collection = JSON.parse(localStorage.getItem('collection'));
     }
-    // Clear input
-    title.value = '';
-    author.value = '';
-    this.displayBooks();
+
+    return collection;
   }
 
-  displayBooks() {
-    const wrapper = document.createElement('div');
-    const line = document.createElement('hr');
-    const bookContainerStr = localStorage.getItem('books');
-    const tits = document.createElement('div');
-    tits.innerText = this.author;
-    const bookArray = JSON.parse(bookContainerStr);
-    bookArray.forEach((element, index) => {
-      const displayTitle = document.createElement('p');
-      const displayAuth = document.createElement('p');
-      const deleteBtn = document.createElement('div');
-      const container = document.createElement('div');
-      const words = document.createElement('div');
-      // set attributes
-      displayTitle.innerText = `"${element.title}" by`;
-      displayAuth.innerText = element.author;
-      deleteBtn.innerHTML = `<button class="btn borders" onclick='deleteItem(${index})'>Remove</button>`;
-      deleteBtn.classList.add('deleteBook');
-      container.classList.add('flexing', 'centers');
-      words.classList.add('flexing');
-      displayAuth.classList.add('word');
-      // apend children
-      words.appendChild(displayTitle);
-      words.appendChild(displayAuth);
-      container.appendChild(words);
-      container.appendChild(deleteBtn);
-      wrapper.appendChild(container);
+  static newBook(book) {
+    const collection = Storage.getBooks();
+    collection.push(book);
+    localStorage.setItem('collection', JSON.stringify(collection));
+  }
+
+  static delBook(author) {
+    const collection = Storage.getBooks();
+
+    collection.forEach((book, index) => {
+      if (book.author === author) {
+        collection.splice(index, 1);
+      }
     });
-    line.classList.add('line');
-    bookList.appendChild(wrapper);
-    bookList.appendChild(line);
-  }
 
-  removeBook(index) {
-    const bookContainerStr = localStorage.getItem('books');
-    const bookArray = JSON.parse(bookContainerStr);
-    bookArray.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(bookArray));
-    bookList.innerHTML = '';
-    this.displayBooks();
+    localStorage.setItem('collection', JSON.stringify(collection));
   }
 }
 
-/* Steve here  */
+class Display {
+  static showBooks() {
+    const collection = Storage.getBooks();
 
-// what happens when a person presses submit
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  // Check if title and author field is empty or not
-  if (title.value === '' || author.value === '') {
-    title.setAttribute('placeholder', 'title');
-    author.setAttribute('placeholder', 'author');
+    collection.forEach((book) => Display.printCollection(book));
+  }
+
+  static printCollection(book) {
+    const bookList = document.querySelector('#books-list');
+
+    const tableRow = document.createElement('tr');
+    const dataCell = document.createElement('td');
+    const dataCell2 = document.createElement('td');
+    const button = document.createElement('button');
+    dataCell.innerHTML = ` <span>"${book.title}"</span> by <span>${book.author}</span> `;
+    tableRow.append(dataCell);
+    tableRow.append(dataCell2);
+    dataCell2.append(button);
+    button.classList.add('delete');
+    button.innerText = 'Remove';
+    bookList.appendChild(tableRow);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', Display.showBooks);
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  if (title === '' || author === '') {
+    Display.showAlert('Enter valid values for title and author fields, please.');
   } else {
-    bookList.innerHTML = '';
-    const book = new Books();
-    book.storeData();
+    const book = new Book(title, author);
+
+    Display.printCollection(book);
+
+    Storage.newBook(book);
+
+    Display.clearFields();
   }
 });
 
-const bigBook = new Books();
-const deleteItem = (id) => {
-  bigBook.removeBook(id);
-};
+document.querySelector('#books-list').addEventListener('click', (e) => {
+  Display.deleteBook(e.target);
 
-// eslint-disable-next-line no-constant-condition
-if ('cl' === 'clz') {
-  deleteItem(1);
-}
-
-window.addEventListener('load', bigBook.displayBooks());
+  Storage.delBook(e.target.parentElement.previousElementSibling.lastElementChild.textContent);
+});
